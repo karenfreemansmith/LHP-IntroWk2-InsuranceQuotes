62a8190c78a3f790141b68a1f9b83d5ed16425dd
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-03-31 10:35:18 -0700

    add validation for age field

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,18 @@ $(document).ready(function() {
   $("#insurance").submit(function(event){
     var age = parseInt($("input#age").val());
     var gender = $("select#gender").val();
-    var quote=(100-age) * 3;
-    if(gender==="male" || age < 26) {
-      quote += 50;
+    if(age) {
+      var quote=(100-age) * 3;
+      if(gender==="male" || age < 26) {
+        quote += 50;
+      }
+
+      $("#rate").empty().append(quote);
+      $("#quote").show();
+    } else {
+      alert("please enter your age.");
     }
 
-    $("#rate").empty().append(quote);
-    $("#quote").show();
 
     event.preventDefault();
   });
