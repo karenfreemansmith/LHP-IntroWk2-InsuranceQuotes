278ba7118105da3f3fabd8f24302429fdd12208c
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-03-31 10:33:26 -0700

    add 0 to men or drivers under 26

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,9 @@ $(document).ready(function() {
     var age = parseInt($("input#age").val());
     var gender = $("select#gender").val();
     var quote=(100-age) * 3;
+    if(gender==="male" || age < 26) {
+      quote += 50;
+    }
 
     $("#rate").empty().append(quote);
     $("#quote").show();
